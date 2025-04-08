2322f0c69fc6e92cfa48b351f4b3f5540639247d
mdrazuahamad <mdrazuahamad8@gmail.com>
2025-04-08 18:20:34 +0600

    Remove unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,6 @@
-import React, { useState } from "react";
 import DynamicForm from "./Component/DynamicForm";
 
 const App = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(formData);
-  };
-
-  const handleChange = (event) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  };
   return (
     <div
       style={{
@@ -30,40 +12,6 @@ const App = () => {
         borderRadius: "8px",
       }}>
       <DynamicForm />
-      {/* <form onSubmit={handleSubmit}>
-        <div>
-          <label>Enter Your Name</label>
-          <input
-            type='text'
-            name='name'
-            placeholder='Enter Your Name'
-            value={formData.name}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Enter Your Email</label>
-          <input
-            type='email'
-            name='email'
-            placeholder='Enter Your Email'
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Enter Your Password</label>
-          <input
-            type='password'
-            name='password'
-            placeholder='Enter Your Password'
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </div>
-
-        <button type='submit'>Submit</button>
-      </form> */}
     </div>
   );
 };
